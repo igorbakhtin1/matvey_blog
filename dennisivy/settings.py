1a65b5e7335e2bb8d0d023a95e03e8b58ce551e6
Igor Bakhtin <48958774+igorbakhtin1@users.noreply.github.com>
2021-05-02 21:09:08 +0300

    Update settings.py

--- dennisivy/settings.py
+++ dennisivy/settings.py
@@ -147,8 +147,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "YOU_EMAIL"
-EMAIL_HOST_PASSWORD = 'YOU_EMAIL_PASSWORD'
+EMAIL_HOST_USER = 'igorbakhtin1@gmail.com'
+EMAIL_HOST_PASSWORD = 'btokkobnpmfysorr'
 
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
